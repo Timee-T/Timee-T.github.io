1749af95b93b7114af0bf3d536f6e80a26341318
Timee-T <thimmy0t@gmail.com>
2020-07-20 01:32:47 -0500

    First commit

---
+++ index.php
@@ -0,0 +1,140 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta name="viewport" content="width=device-width,initial-scale=1.0,user-scalable=0">
+	<link rel="stylesheet" type="text/css" href="css/font-awesome.css">
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<title>Quote.com</title>
+</head>
+<body>
+	<nav class="hidden-sm show-md hidden-xs">
+		<div class="mother">
+			<div class="sm-col-5 centered">
+				<div class="sm-col-6">
+					<img src="images/Quote.png" class="nav-img-container">
+				</div>
+				<div class="sm-col-6 left">
+					<a href="">Login</a>
+				</div>
+			</div>
+			<div class="sm-col-7 centered">
+				<a href="">Home</a>
+				<a href="">Inbox</a>
+			</div>
+		</div>
+	</nav>
+	<div class="hidden-md">
+		<div class="mobile-nav">
+			<a href="javascript:void(0)" onclick="sideMenuController()"><i class="fa fa-bars"></i></a>
+		</div>
+		<div class="mobile-menu hidden" id="side-bar">
+			<a href="">Home</a>
+			<a href="">About</a>
+			<a href="">Contact us</a>
+			<a href="">Repo</a>
+		</div>
+		<div class="dark-underlay hidden" onclick="closeMenu()" id="dark-underlay"></div>
+	</div>
+	<div class="bg-1">
+		<div class="scarf">
+			<div class="xs-col-12 xs-down-80 sm-col-5 sm-off-6 sm-down-85 sm-centered">
+				<div class="xs-container xs-centered">
+					<div class="color-code-2"><span class="white px30"><i class="fa fa-circle"></i> WELCOME</span></div>
+					<div class="xs-down-10 sm-left"><span class="white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></div>
+				</div>
+			</div>
+			<div>
+				<div class="hidden-xs sm-col-5 sm-off-1 sm-down-85 lg-down-90 sm-centered">
+					<div class="left color-code-2"><span class="white px30"><i class="fa fa-circle"></i> WELCOME</span></div>
+					<div class="left"><span class="white">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+					tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+					quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+					consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+					cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+					proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</span></div>
+				</div>
+				<div class="sm-col-5 xs-col-12 xs-down-40">
+					<div class="sm-col-12 sm-down-70">
+						<div class="container">
+							<div><img src="images/img-2.jpg" class="img-container sm-height-200"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<div class="mother">
+		<div class="sm-container xs-container">
+			<h1 class="color-code-2">Michelangelo Buoarroti</h2>
+			<div class="sm-col-6 lg-down-5 pre-scrollable xs-container">
+				Michelangelo Buonarroti was born on March 6, 1475, in Caprese, a town in Tuscany. His parents were Lodovico Buonarroti Simoni, the podesta, or mayor, of Caprese, and Francesca di Neri, who died when Michelangelo was six. The Buonarroti family was descended from Florentine nobility, but its financial and social positions had been seriously compromised by the time Michelangelo was born. A month after Michelangelo's birth, the family returned to Florence, where Michelangelo was entrusted to a wet-nurse.
+
+				In 1485, Lodovico Buonarroti remarried, and Michelangelo returned to Florence to live with his family, briefly attending a local school. Although his father did not approve, Michelangelo became an apprentice in the studio of Domenico and David Ghirlandaio, where he made sketches of Early Renaissance works and probably learned fresco painting. In early 1489, he left the Ghirlandaio brothers' studio to enroll in Bertoldo di Giovanni's school of sculpture, where he worked on clay and marble copies of Classical works.
+
+				In 1490, the fifteen-year-old Michelangelo's talent was so advanced that Lorenzo de' Medici, known as "The Magnificent", an important Florentine patron of the arts, invited the young artist to live in his palace. Michelangelo stayed in the de' Medici home until Lorenzo died in April 1492, and his time there proved extremely important to his development. In Lorenzo's palace, Michelangelo was able to continue his academic education on an informal basis, and he was exposed to both the leading champion of Neoplatonism, Marsilio Ficino, and to its greatest detractor, the religious fanatic Girolamo Savonarola. In 1492, Michelangelo was invited back to the Medici palace by Piero de'Medici, where he worked until the French invaded under Charles VII in 1494, whereupon he fled to Venice and Bologna. Florence briefly became a republic, and Michelangelo returned in 1495 to work at the Medici palace.
+
+				In the summer of 1496, Michelangelo moved to Rome, partly to avoid the delicate political situation in Florence. He remained in Rome until 1501, working on several important commissions for sculptures, among them an early pieta and a Bacchus. Michelangelo's fame grew, and he returned to Florence in 1501, where he received a contract for the marble David, which was not completed until 1504. He began several other contracts for sculptural figures and paintings, but most of these commissions were never finished.
+
+				Pope Julius II summoned Michelangelo to Rome in 1505 to begin work on his tomb. The Tomb of Julius project, with its numerous redesigns and nagging contract problems, was completed in a scaled-down version forty years later, and Michelangelo considered it the low point of his career. In 1506, Michelangelo left Rome for Florence to work on an unfinished mural, then went to Bologna, where he executed a bronze bust of the Pope. In 1508, Julius II asked Michelangelo back to Rome to paint the ceiling of the Sistine Chapel, an aggravating assignment that he finished in 1512. After Pope Julius II died in 1512, Michelangelo remained in Rome until 1516, ironing out contract terms for the tomb and slowly beginning work on it.
+
+				In 1516, Michelangelo returned to Florence, which was again under Medici power, at the behest of the new Pope, Leo X. There he began work on a fa¸ade for the San Lorenzo cathedral. Michelangelo worked intermittently on this project and on the Julius tomb in Rome until 1520. From 1520 to 1524, during the beginning of the Reformation, he worked on a project for the Medicis, but the family fled the city in 1529, when the papal seat of Rome was sacked by mercenaries of the Holy Roman Empire. Michelangelo spent 1529 in the employ of the Florentine Republic, working on designing fortifications to defend the city from invasion. As the conflict between the deposed Pope Clement VII and the Florentine republic worsened, Michelangelo fled to Venice, but returned to Florence when the republic accused him of treason. In 1530, Florence was occupied by imperial troops, who put the city under the control of Clement VII. The Pope offered a frightened Michelangelo immunity if he continued to work on the Medici Chapel, which he did until 1532, when he went to Rome for more tomb contract negotiations. He remained in Rome for the rest of his life. After Pope Paul III succeeded Clement VII in 1534, he ordered Michelangelo to halt construction of the tomb of Julius to begin the Last Judgement altarpiece for the Sistine Chapel in Rome. After the completion of the Last Judgement in 1541, Michelangelo began work on his St. Paul and St. Peter frescoes for Paul III, and finally finished the tomb of Julius, though he was disappointed with the result.
+
+				At this time, Michelangelo came under attack for his use of nudes in the Last Judgement. Feeling the effects of the Counter-Reformation, Michelangelo began to turn more to architecture, and he also began to write poetry in earnest. In the late 1540s, Michelangelo took on several important projects, including the construction of the Palazzo Farnese, sculpting the Florentine Pieta, rebuilding Rome's Campidoglio, and designing St. Peter's in Rome. Vasari's biography of Michelangelo was published in 1550, and Condivi's followed in 1553.
+
+				In the last decade of his life, Michelangelo worked simultaneously on a number of projects, most notably three statues of the pieta, a series of religious drawings, as well as designs for the dome of St. Peter's and several other important buildings. As his health failed, Michelangelo concentrated on design drawings and models for his architectural projects, and worked slowly on his pietas. In February 1564, he suddenly became very ill, and he died two days later, with his close friends by his bed. Although the Pope desired his body to be buried in St. Peter's, Michelangelo's heir brought it back to Florence, where it was buried in Santa Croce.
+			</div>
+			<div class="xs-col-12 sm-col-6">
+				<div class="card  lg-down-5 xs-down-40">
+					<div class="sm-container xs-container pre-scrollable">
+						<div class="sm-centered px30 color-code-2">
+							Michelangelo's Works
+						</div>
+						<div class="sm-down-20 xs-down-10">
+							<div class="img-container"><img src="images/michelangelo-1.jpg"></div>
+							<div class="sm-centered xs-centered xs-down-10 sm-down-10">
+								<span>Ceiling Fresco by Michelangelo</span>
+							</div>
+						</div>
+						<div class="sm-down-20 xs-down-20">
+							<div class="img-container"><img src="images/michelangelo-2.jpg"></div>
+							<div class="sm-centered sm-down-10 xs-centered xs-down-10">
+								<span>Pieta Marble structure by Michelangelo</span>
+							</div>
+						</div>
+						<div class="sm-down-20 xs-down-20">
+							<div class="img-container"><img src="images/michelangelo-3.jpg"></div>
+							<div class="sm-centered sm-down-10 xs-centered xs-down-10">
+								<span>Pieta Marble structure by Michelangelo</span>
+							</div>
+						</div>
+						<div class="sm-down-20 xs-down-20">
+							<div class="img-container"><img src="images/michelangelo-4.jpg"></div>
+							<div class="sm-centered sm-down-10 xs-centered xs-down-10">
+								<span>Pieta Marble structure by Michelangelo</span>
+							</div>
+						</div>
+						<div class="sm-down-20 xs-down-20">
+							<div class="img-container"><img src="images/michelangelo-5.jpg"></div>
+							<div class="sm-centered sm-down-10 xs-centered xs-down-10">
+								<span>Pieta Marble structure by Michelangelo</span>
+							</div>
+						</div>
+						<div class="sm-down-20 xs-down-20">
+							<div class="img-container"><img src="images/michelangelo-6.jpg"></div>
+							<div class="sm-centered sm-down-10 lg-down-10 xs-centered">
+								<span>Pieta Marble structure by Michelangelo</span>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
+<script type="" src="js/script.js"></script>
+</html>
