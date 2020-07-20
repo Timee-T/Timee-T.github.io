e1cfa4524a79d60d5436f5bafe5a845581854110
Timee-T <thimmy0t@gmail.com>
2020-07-20 02:19:46 -0500

    Fifth commit js/

---
+++ js/script.js
@@ -0,0 +1,12 @@
+function sideMenuController(){
+	var x = document.getElementById('side-bar')
+	x.style.display = 'block';
+	var y = document.getElementById('dark-underlay')
+	y.style.display = 'block'
+}
+function closeMenu(){
+	var x = document.getElementById('side-bar')
+	x.style.display = 'none'
+	var y =  document.getElementById('dark-underlay')
+	y.style.display = 'none'
+}
